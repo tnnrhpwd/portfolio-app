a19b6c75099b6a2dae0e4397c014778390079a15
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-04-26 16:09:31 -0500

    removed unnecessary imports
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/Home/BackgroundVideo.js
+++ src/components/Home/BackgroundVideo.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-let bgVideo;
+import React from "react";
+
 
 // https://www.amazon.com/drive/v1/nodes/7zdVKb3AQU6FiBAyESKxvw/contentRedirection?querySuffix=%3FvideoTranscodeView%3Dtrue%26y%3D720&ownerId=A2ED9145G3H0AN&cb=1649615247945&groupShareToken=xLbJljPqTCy3dJzMbd9VxA.7873MSVWGwCNWq9bhReGhp
 
