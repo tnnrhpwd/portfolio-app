48c3be44705ec5371e2b4fa71a75151df69713e5
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-04-28 10:44:43 -0500

    NavItem -- Changed the jsx component from list to div
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/NavBar/NavItem.js
+++ src/components/NavBar/NavItem.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 function NavItem(props){
     return(
-        <li className="nav-item">
+        <div className="nav-item">
             <a href={props.page} className="icon-button">
                 <img src={props.icon} className="nav-icon" alt="the png logos for the links to each webpage"/>
             </a>
-        </li>
+        </div>
     );
 }
 
