7f20ca2d19f6ed657039a6a26396965823146896
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-04-28 10:46:06 -0500

    Added react-tooltips to each of the navbar components.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import React from "react";
-import {Link} from 'react-router-dom';
 import NavItem from "./NavItem";
 import NavLogo from "./NavLogo";
+import ReactTooltip from "react-tooltip";
 // import icons  --may change to SVGs in the future
 import homeLogo from './home.png';
 import projectsLogo from './projects.png';
 import contactLogo from './contact.png';
 import STHlogo from './STHlogo192.png';
 import './NavBar.css';
+
 
 function NavBar({handleLoginClick}) { 
 
@@ -23,10 +24,28 @@ function NavBar({handleLoginClick}) {
             <NavLogo icon={STHlogo}/>
           </span>
           <ul className="navbar-nav"> 
-          {/* {props.children} */}
-          <NavItem icon={homeLogo} page="/"/>
-          <NavItem icon={projectsLogo} page="/projects"/>
-          <NavItem icon={contactLogo} page="/contact"/>
+            <div className="tooltip-space" data-tip="" data-for="tooltip-home" >
+              <NavItem  icon={homeLogo} page="/"/>
+            </div>
+            <ReactTooltip id="tooltip-home" place="bottom" effect="solid">
+              Home
+            </ReactTooltip>
+
+            <div className="tooltip-space" data-tip="" data-for="tooltip-projects" >
+              <NavItem icon={projectsLogo} page="/projects"/>
+            </div>
+            <ReactTooltip id="tooltip-projects" place="bottom" effect="solid">
+              Projects
+            </ReactTooltip>
+
+            <div className="tooltip-space" data-tip="" data-for="tooltip-contact" >
+              <NavItem icon={contactLogo} page="/contact"/>
+            </div>
+            <ReactTooltip id="tooltip-contact" place="bottom" effect="solid">
+              Contact
+            </ReactTooltip>
+            
+            
           </ul>
       </nav>
     );
