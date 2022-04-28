ed87662c57a6fc7514c1521566b6b259de56e0e7
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-04-28 13:12:04 -0500

    APP -- Added alcohol calculator to routes.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home/Home';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import NavBar from './components/NavBar/NavBar';
-import LoginForm from './components/LoginForm/LoginForm';
-import ProdPartners from './components/ProdPartners/ProdPartners';
-import PollBox from './components/PollBox/PollBox';
-import Drafting from "./components/Drafting/Drafting";
+
+import Alcohol from './components/Alcohol/Alcohol';
 import Annuities from './components/Annuities/Annuities';
+import Contact from './components/Contact/Contact';
+import Drafting from "./components/Drafting/Drafting";
+import Home from './components/Home/Home';
+import LoginForm from './components/LoginForm/LoginForm';
+import NavBar from './components/NavBar/NavBar';
+import PollBox from './components/PollBox/PollBox';
+import ProdPartners from './components/ProdPartners/ProdPartners';
+import Projects from './components/Projects/Projects';
 import Wordle from './components/Wordle/Wordle';
 import WordleSolver from './components/WordleSolver/WordleSolver';
 
@@ -25,12 +27,13 @@ function App() {
       <LoginForm isShowLogin={isShowLogin} />
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/prodpartners" element={<ProdPartners/>} />
-        <Route path="/pollbox" element={<PollBox/>} />
-        <Route path="/drafting" element={<Drafting/>} />
+        <Route path="/alcohol" element={<Alcohol/>} />
         <Route path="/annuities" element={<Annuities/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/drafting" element={<Drafting/>} />
+        <Route path="/prodpartners" element={<ProdPartners/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/pollbox" element={<PollBox/>} />
         <Route path="/wordlesolver" element={<WordleSolver/>} />
         <Route path="/wordle" element={<Wordle/>} />
       </Routes>
