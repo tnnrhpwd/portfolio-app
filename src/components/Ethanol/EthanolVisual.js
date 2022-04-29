a088c2146f7be0bf6b12e992347a447cffc07ff0
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-04-29 14:31:04 -0500

    Ethanol -- made the styling look better on 4K displays. Resized inputs, column widths, etc.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/Ethanol/EthanolVisual.js
+++ src/components/Ethanol/EthanolVisual.js
@@ -3,33 +3,25 @@ import {CircularProgressbar} from "react-circular-progressbar";
 import 'react-circular-progressbar/dist/styles.css';
 
 function EthanolVisual(props) {
+    var circleArray = [];    // temporary array
+    const [stateArray, setStateArray] = useState([]);    // output array
 
-    var circleArray = [];
-
-    const [stateArray, setStateArray] = useState([]);
-
-    // props.out
-
-   
-
-    useEffect(() => {
-        
+    useEffect(() => {   // RUN on EACH INPUT
+        // add input integer number of circles.
         for(let i = 0; i < Math.floor(props.out); i++){
             circleArray.push(<div className='ethanolvisual-circles' key={i} ><CircularProgressbar key={i} value={100}/></div>)
             if( i%10===0 && i!==0 ){circleArray.push(<br/>)}
         }
 
-
         let decimal;
-        if(!Number.isInteger(props.out)){
-            decimal = props.out - Math.floor(props.out);
+         // if input exists && has decimal ending, add partial circle
+        if(!Number.isInteger(props.out) && props.out > 0){ 
+            decimal = (props.out - Math.floor(props.out)) * 100;
             circleArray.push(<div className='ethanolvisual-circles' key={11111} ><CircularProgressbar key={-1} value={decimal}/></div>)
         }
 
-
-
+        // output array
         setStateArray(circleArray);
-        console.log("test")
     },[props.out]);
 
     return (
