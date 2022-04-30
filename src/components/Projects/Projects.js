be0ed28eeb262c4b2069396e19446f0232060dd7
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-04-30 17:42:10 -0500

    Made the project images smaller. Images can now wrap into a grid on big devices.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,54 +21,56 @@ function Projects() {
           Click each project for more information.
         </div>
         <div></div>
+        <div className="projects-holder">
 
-        <div className="projects-p1">
-        
-        <a href="/pollbox">
-          <img id="projects-p1-img" src={pollsimg} alt="the display for polling website"/>
-        </a>
-        <div className="projects-p1-text">
-        </div>
-      </div>
-      <div></div>
+          <div className="projects-p1">
+            <a href="/pollbox">
+              <img id="projects-p1-img" src={pollsimg} alt="the display for polling website"/>
+            </a>
+          </div>
 
-        <div className="projects-p5">
-          <a href="/annuities">
-            <img id="projects-p5-img" src={annuitiesimg} alt="the display for my cad projects"/>
-          </a>
-        </div>
-        <div></div>
 
-        <div className="projects-p6">
-          <a href="/wordle">
-            <img id="projects-p6-img" src={wordleimg} alt="the display for wordle "/>
-          </a>
-        </div>
-        <div></div>
+          <div className="projects-p5">
+            <a href="/annuities">
+              <img id="projects-p5-img" src={annuitiesimg} alt="the display for my cad projects"/>
+            </a>
+          </div>
 
-        <div className="projects-p2">
-          <a href="/wordlesolver">
-            <img id="projects-p2-img" src={wordleSimg} alt="the display for wordle solver website"/>
-          </a>
-        </div>
-        <div></div>
 
-        <div className="projects-p7">
-          <a href="/ethanol">
-            <img id="projects-p7-img" src={ethanolimg} alt="the display for ethanol calculator"/>
-          </a>
-        </div>
-        <div></div>
+          <div className="projects-p6">
+            <a href="/wordle">
+              <img id="projects-p6-img" src={wordleimg} alt="the display for wordle "/>
+            </a>
+          </div>
 
-        <div className="projects-p3">
-          <BitcoinAPI/>
+
+          <div className="projects-p2">
+            <a href="/wordlesolver">
+              <img id="projects-p2-img" src={wordleSimg} alt="the display for wordle solver website"/>
+            </a>
+          </div>
+
+
+          <div className="projects-p7">
+            <a href="/ethanol">
+              <img id="projects-p7-img" src={ethanolimg} alt="the display for ethanol calculator"/>
+            </a>
+          </div>
+
+
+          <div className="projects-p3">
+            <BitcoinAPI/>
+          </div>
+
+          <div className="projects-p4">
+            <a href="/drafting">
+              <img id="projects-p4-img" src={draftingimg} alt="the display for my cad projects"/>
+            </a>
+          </div>
+
+          
         </div>
-        <div></div>
-        <div className="projects-p4">
-          <a href="/drafting">
-            <img id="projects-p4-img" src={draftingimg} alt="the display for my cad projects"/>
-          </a>
-        </div>
+
         <div></div>
         <div className="projects-resume">
           <a className="projects-resume-inside" href="https://docs.google.com/document/d/1l8yCRlom5hw-SwOfZtpria_AUuXwcXpC/edit?usp=sharing&ouid=106668374323360993837&rtpof=true&sd=true">
