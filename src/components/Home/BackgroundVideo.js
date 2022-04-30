b2ab160ee2a7419b4aeb48732ae5bc21c0fa59cd
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-04-30 17:37:38 -0500

    Added loop back to video
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/Home/BackgroundVideo.js
+++ src/components/Home/BackgroundVideo.js
@@ -18,7 +18,7 @@ function BackgroundVideo(props){
     return(<>
         <div id="set-height"></div>
         <div className="videoHome">
-            <video id="video1" muted autoPlay>
+            <video id="video1" muted loop autoPlay>
                 <source src="https://www.amazon.com/drive/v1/nodes/7zdVKb3AQU6FiBAyESKxvw/contentRedirection?querySuffix=%3FvideoTranscodeView%3Dtrue%26y%3D720&ownerId=A2ED9145G3H0AN&cb=1649615247945&groupShareToken=xLbJljPqTCy3dJzMbd9VxA.7873MSVWGwCNWq9bhReGhp" type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;" />
             </video>
         </div>
