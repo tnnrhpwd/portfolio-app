7f3ac710300278c24de9b9365a1473cd2574223c
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-05-01 13:37:39 -0500

    Initial page commit.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

---
+++ src/components/Planit/Planit.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import planitimg from './planit.png';
+
+import './Planit.css';
+
+function Planit() {
+    return (<>
+        <div className='planit-space'>
+            <nav className='planit-navbar'>
+                <img id="planit-navbar-logo" src={planitimg} alt="planit logo"/>
+            </nav>
+            <div className='planit-title'>
+                Planit
+            </div>
+
+        </div>
+    </>)
+}
+
+export default Planit
