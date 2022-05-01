280b3e690302a77ef3dfbb96b03c3f1912c732ae
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-05-01 13:37:25 -0500

    Reordered routes to be alphabetical. Added Planit page.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 
 import Annuities from './components/Annuities/Annuities';
 import Contact from './components/Contact/Contact';
 import Drafting from "./components/Drafting/Drafting";
 import Ethanol from './components/Ethanol/Ethanol';
 import Home from './components/Home/Home';
-
+import Planit from './components/Planit/Planit';
 import PollBox from './components/PollBox/PollBox';
 import ProdPartners from './components/ProdPartners/ProdPartners';
 import Projects from './components/Projects/Projects';
 import Wordle from './components/Wordle/Wordle';
 import WordleSolver from './components/WordleSolver/WordleSolver';
 
+import './App.css';
+
 function App() {
-
-
   return (
     <div className="App">
-      
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/ethanol" element={<Ethanol/>} />
         <Route path="/annuities" element={<Annuities/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/drafting" element={<Drafting/>} />
+        <Route path="/ethanol" element={<Ethanol/>} />
+        <Route path="/planit" element={<Planit/>} />
+        <Route path="/pollbox" element={<PollBox/>} />
         <Route path="/prodpartners" element={<ProdPartners/>} />
         <Route path="/projects" element={<Projects/>} />
-        <Route path="/pollbox" element={<PollBox/>} />
-        <Route path="/wordlesolver" element={<WordleSolver/>} />
         <Route path="/wordle" element={<Wordle/>} />
+        <Route path="/wordlesolver" element={<WordleSolver/>} />
       </Routes>
     </div>
   );
