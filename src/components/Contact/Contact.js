0a2e5a4b23cb0a39e44a44fba687588c26c03e79
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-05-01 12:04:56 -0500

    Re-added NavBar
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,50 +1,55 @@
 import React from "react";
-import "./Contact.css";
 import linkedinimg from "./linkedin.png";
 import githubimg from "./githubW.png";
 import Footer from './../Footer/Footer';
+import NavBar from './../NavBar/NavBar';
+import "./Contact.css";
 
 function Contact() {
-    return (<div className="contact-space">
-      <div className="contact-title">
-          Contact
-      </div>
-      <div className="contact-name">
-        Steven Tanner Hopwood
-      </div>
-      <div className="contact-email">
-        Steven.T.Hopwood@gmail.com
-      </div>
-      <div className="contact-body">
-        <td className="contact-social" onClick={()=> window.open("https://www.linkedin.com/in/sthopwood/", "_blank")}>
-          <img className="contact-linkedin" src={linkedinimg} alt=" linkedin logo" />
-        </td>
-        <td className="contact-social" onClick={()=> window.open("https://github.com/tnnrhpwd", "_blank")}>
-          <img className="contact-github" src={githubimg} alt="github logo" />
-        </td>
-      </div>
-      <div className="contact-body">
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" netlify>
-      <input type="hidden" name="form-name" value="contact" />  
-        <div>
-          <label className="contact-input-text">Name </label>
-          <input className="contact-input-contact" type="text" name="name" />
+    return (
+    <>
+      <NavBar/>
+      <div className="contact-space">
+        <div className="contact-title">
+            Contact
         </div>
-        <div>
-          <label className="contact-input-text">Email (not required) </label>
-          <input className="contact-input-contact" type="text" name="email" />
+        <div className="contact-name">
+          Steven Tanner Hopwood
         </div>
-        <div>
-          <label className="contact-input-text">Message </label>
-          <textarea className="contact-input-message" name="message"></textarea>
+        <div className="contact-email">
+          Steven.T.Hopwood@gmail.com
         </div>
-        <div className="contact-link-div">
-          <button id="contact-link" type="submit">Submit</button>
+        <div className="contact-body">
+          <td className="contact-social" onClick={()=> window.open("https://www.linkedin.com/in/sthopwood/", "_blank")}>
+            <img className="contact-linkedin" src={linkedinimg} alt=" linkedin logo" />
+          </td>
+          <td className="contact-social" onClick={()=> window.open("https://github.com/tnnrhpwd", "_blank")}>
+            <img className="contact-github" src={githubimg} alt="github logo" />
+          </td>
         </div>
-      </form>
+        <div className="contact-body">
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" netlify>
+        <input type="hidden" name="form-name" value="contact" />  
+          <div>
+            <label className="contact-input-text">Name </label>
+            <input className="contact-input-contact" type="text" name="name" />
+          </div>
+          <div>
+            <label className="contact-input-text">Email (not required) </label>
+            <input className="contact-input-contact" type="text" name="email" />
+          </div>
+          <div>
+            <label className="contact-input-text">Message </label>
+            <textarea className="contact-input-message" name="message"></textarea>
+          </div>
+          <div className="contact-link-div">
+            <button id="contact-link" type="submit">Submit</button>
+          </div>
+        </form>
+        </div>
+        <Footer/>
       </div>
-      <Footer/>
-    </div>);
+    </>);
   }
   
   export default Contact;
