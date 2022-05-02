82e90f9263086ecc0a29a9d2cfb5319d894d089b
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-05-02 13:09:42 -0500

    Now, the video only loads and plays when the device is in landscape. This will prevent video loads in most mobile circumstances.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/Home/BackgroundVideo.js
+++ src/components/Home/BackgroundVideo.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 
 // https://www.amazon.com/drive/v1/nodes/7zdVKb3AQU6FiBAyESKxvw/contentRedirection?querySuffix=%3FvideoTranscodeView%3Dtrue%26y%3D720&ownerId=A2ED9145G3H0AN&cb=1649615247945&groupShareToken=xLbJljPqTCy3dJzMbd9VxA.7873MSVWGwCNWq9bhReGhp
 
 function BackgroundVideo(props){
 
-    // useEffect(()=>{
-    //     if(props.renN==2){
-    //         bgVideo = document.getElementById("video1");
-    //     }
-    //     if(props.renN>2){
-    //         // bgVideo.currentTime = props.yOff;
-    //         console.log('frame='+bgVideo.currentTime+" out of "+bgVideo.duration+" frames.");
-    //     }
-    // },[props.renN]);
+    let autoplay = false;
+    window.onload = addAutoplay();
+    function addAutoplay(){
+        if(window.matchMedia("(orientation: landscape)").matches) {
+            autoplay=true;
+        }
+    }
 
-    return(<>
-        <div id="set-height"></div>
-        <div className="videoHome">
-            <video id="video1" muted loop autoPlay>
-                <source src="https://www.amazon.com/drive/v1/nodes/7zdVKb3AQU6FiBAyESKxvw/contentRedirection?querySuffix=%3FvideoTranscodeView%3Dtrue%26y%3D720&ownerId=A2ED9145G3H0AN&cb=1649615247945&groupShareToken=xLbJljPqTCy3dJzMbd9VxA.7873MSVWGwCNWq9bhReGhp" type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;" />
-            </video>
-        </div>
-    </>);
+    if(autoplay) {
+        return(<>
+            <div id="set-height"></div>
+            <div className="videoHome">
+                <video id="video1" muted loop autoPlay  >
+                    <source src="https://www.amazon.com/drive/v1/nodes/7zdVKb3AQU6FiBAyESKxvw/contentRedirection?querySuffix=%3FvideoTranscodeView%3Dtrue%26y%3D720&ownerId=A2ED9145G3H0AN&cb=1649615247945&groupShareToken=xLbJljPqTCy3dJzMbd9VxA.7873MSVWGwCNWq9bhReGhp" type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;" />
+                </video>
+            </div>
+        </>);
+    } else {
+        return(<>
+            <div id="set-height"></div>
+            <div className="videoHome">
+                <video id="video1" muted loop preload="none" >
+                    <source src="https://www.amazon.com/drive/v1/nodes/7zdVKb3AQU6FiBAyESKxvw/contentRedirection?querySuffix=%3FvideoTranscodeView%3Dtrue%26y%3D720&ownerId=A2ED9145G3H0AN&cb=1649615247945&groupShareToken=xLbJljPqTCy3dJzMbd9VxA.7873MSVWGwCNWq9bhReGhp" type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;" />
+                </video>
+            </div>
+        </>);
+    }
 }
 
 export default BackgroundVideo;
