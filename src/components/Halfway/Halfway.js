c1e59308204136d72565b913b803cdeb7676a3b8
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-09-08 20:10:35 -0500

    Added basic halfway calculator functionality, but the return value is wrong.
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/components/Halfway/Halfway.js
+++ src/components/Halfway/Halfway.js
@@ -4,7 +4,18 @@ import NavBar from './../NavBar/NavBar';
 import './Halfway.css';
 
 function Halfway() {
-    const [destinationTime, setDestinationTime] = useState("");
+    const [endTime, setEndTime] = useState(0);
+    const [startTime, setStartTime] = useState(0);
+    const [halfwayTime, setHalfwayTime] = useState(0);
+
+    function handleSubmit(){
+        // GUARD CLAUSE --
+        if((endTime===startTime)){ return; }
+
+        let answer = parseInt((endTime-startTime)/2)+parseFloat(startTime)
+
+        setHalfwayTime(answer);
+    }
 
     return (<>
         <NavBar/>
@@ -15,6 +26,30 @@ function Halfway() {
             <div className="halfway-description">
                 This calculator estimates the halfway time of any trip. 
             </div>
+
+            <div className='halfway-col1'>
+                <div className='halfway-calculator'>
+                    <div className='halfway-calculator-title'>
+                        Time Calculator
+                    </div>
+                    <div className='halfway-calculator-start'>
+                        <div className="halfway-calculator-input-title">
+                            Start Time:
+                        </div>
+                        <input id="halfway-calculator-input" placeholder="1400" onChange={e => setStartTime(e.target.value)} type="text"/>
+                    </div>
+                    <div className='halfway-calculator-end'>
+                        <div className="halfway-calculator-input-title">
+                            End Time:
+                        </div>
+                        <input id="halfway-calculator-input" placeholder="1950" onChange={e => setEndTime(e.target.value)} type="text"/>
+                    </div>
+
+                    <button id="ethanol-calculator-submit" onClick={handleSubmit}>Submit</button>
+                    { halfwayTime }
+                </div>
+            </div>
+
 
             <br/>
             <a href="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/Halfway" rel="noreferrer"  target="_blank">
