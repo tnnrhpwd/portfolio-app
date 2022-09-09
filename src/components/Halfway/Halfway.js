8784216de874d2dfddce1d6d59dc1d75c3a23626
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-09-08 19:21:25 -0500

    halfway page init commit
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

---
+++ src/components/Halfway/Halfway.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import planitimg from './planit.png';
+
+import './Halfway.css';
+
+function Halfway() {
+    return (<>
+        <div className='halfway-space'>
+            <nav className='halfway-navbar'>
+                {/* <img id="halfway-navbar-logo" src={planitimg} alt="halfway logo"/> */}
+            </nav>
+            <div className='halfway-title'>
+                Halfway
+            </div>
+
+        </div>
+    </>)
+}
+
+export default Halfway
