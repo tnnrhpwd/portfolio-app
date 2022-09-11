7cf2f4804ccea8fcc3ddee7ebe804032bc234af5
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-09-11 15:30:54 -0500

    test to pass compile
    Signed-off-by: Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Annuities from './components/Annuities/Annuities';
 import Contact from './components/Contact/Contact';
 import Drafting from "./components/Drafting/Drafting";
 import Ethanol from './components/Ethanol/Ethanol';
-import Halfway from './components/Halfway/Halfway';
+// import Halfway from './components/Halfway/Halfway';
 import Home from './components/Home/Home';
 import Planit from './components/Planit/Planit';
 import PollBox from './components/PollBox/PollBox';
@@ -25,7 +25,7 @@ function App() {
         <Route path="/contact" element={<Contact/>} />
         <Route path="/drafting" element={<Drafting/>} />
         <Route path="/ethanol" element={<Ethanol/>} />
-        <Route path="/halfway" element={<Halfway/>} />
+        {/* <Route path="/halfway" element={<Halfway/>} /> */}
         <Route path="/planit" element={<Planit/>} />
         <Route path="/pollbox" element={<PollBox/>} />
         <Route path="/prodpartners" element={<ProdPartners/>} />
