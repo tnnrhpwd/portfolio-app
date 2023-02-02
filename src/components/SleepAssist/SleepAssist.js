0f758366a83a50dcafe2386d5d98bba15ea8a9da
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2023-02-01 19:45:39 -0600

    implemented the sleep assist code
    implemented the sleep assist code

--- src/components/SleepAssist/SleepAssist.js
+++ src/components/SleepAssist/SleepAssist.js
@@ -4,30 +4,18 @@ import NavBar from '../NavBar/NavBar';
 import { getSunrise, getSunset } from 'sunrise-sunset-js';
 import './SleepAssist.css';
 
-function Halfway() {
-    const [endTime, setEndTime] = useState(0);
-    const [startTime, setStartTime] = useState(0);
-    const [halfwayTime, setHalfwayTime] = useState(0);
+function SleepAssist() {
+    const [wakeTime, setWakeTime] = useState(0);
+    const [bedTime, setBedTime] = useState(0);
     const [currentTime, setCurrentTime] = useState(0);
-    const [sunriseTime, setSunriseTime] = useState("");
-    const [sunsetTime, setSunsetTime] = useState("");
 
     function handleSubmit(){
         // GUARD CLAUSE --
-        if((endTime===startTime)){ return; }
-        let answer = parseInt((endTime-startTime)/2)+parseFloat(startTime) // calculate halfway point
-        setHalfwayTime(answer);
-    }
-    function getSunTimesFromLocation(){
-        // Combined with geolocation. Sunset tonight at your location.
-        navigator.geolocation.getCurrentPosition(function(position) {
-            setSunriseTime( getSunrise(position.coords.latitude, position.coords.longitude) );
-            setSunsetTime( getSunset(position.coords.latitude, position.coords.longitude) );
-        });
+        if((wakeTime===bedTime)){ return; }
+
     }
     function getAllTimes(){
         setCurrentTime(new Date())
-        getSunTimesFromLocation()
     }
 
     useEffect(() => {
@@ -36,59 +24,24 @@ function Halfway() {
     
     return (<>
         <NavBar/>
-        <div className='halfway'>
-            <div className="halfway-title">
-                Halfway
+        <div className='sleepassist'>
+            <div className="sleepassist-title">
+                SleepAssist
             </div>
-            <div className="halfway-description">
-                This calculator estimates the halfway time of any trip. 
+            <div className="sleepassist-description">
+                This calculator inputs alarm time & sleep duration, and it outputs the time to go to sleep. 
             </div>
 
-            <div className='halfway-col1'>
-                <div className='halfway-calculator'>
-                    <div className='halfway-calculator-title'>
-                        Time Calculator
-                    </div>
+            <div className='sleepassist-col1'>
+               <input placeholder='alarm time ex. 500'></input>
+               <input placeholder='sleep duration ex. 800'></input>
 
-
-
-                    <div className='halfway-calculator-start'>
-                        <div className='halfway-calculator-input-title'>
-                            <div>Currently:</div>
-                            { currentTime.toString() }
-                            <div>Sunrise</div>
-                            { sunriseTime.toString() }
-                            <div>Sunset</div>
-                            { sunsetTime.toString() }
-                            <button id="ethanol-calculator-submit" onClick={setStartTime(currentTime)}>current</button>
-                            {/* <button id="ethanol-calculator-submit" onClick={}>sunrise</button>
-                            <button id="ethanol-calculator-submit" onClick={}>sunset</button> */}
-                            
-                        </div>
-                        <div className="halfway-calculator-input-title">
-                            Start Time:
-                        </div>
-                        <input id="halfway-calculator-input" placeholder="1400" onChange={e => setStartTime(e.target.value)} type="text"/>
-                    </div>
-                    <div className='halfway-calculator-end'>
-                        <div className='halfway-calculator-input-title'>
-                            { sunsetTime.toString() }
-                        </div>
-                        <div className="halfway-calculator-input-title">
-                            End Time:
-                        </div>
-                        <input id="halfway-calculator-input" placeholder="1950" onChange={e => setEndTime(e.target.value)} type="text"/>
-                    </div>
-
-                    <button id="ethanol-calculator-submit" onClick={handleSubmit}>Submit</button>
-                    { halfwayTime }
-                    
-                </div>
+                <div>the output</div>
             </div>
 
             <br/>
-            <a href="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/Halfway" rel="noopener noreferrer"  target="_blank">
-                <button id="halfway-sourcecode">View Source Code</button>
+            <a href="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/SleepAssist" rel="noopener noreferrer"  target="_blank">
+                <button id="sleepassist-sourcecode">View Source Code</button>
             </a>
 
         </div>
@@ -96,4 +49,4 @@ function Halfway() {
     </>)
 }
 
-export default Halfway
+export default SleepAssist
