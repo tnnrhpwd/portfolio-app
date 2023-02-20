90d380fcec95dc1c62a91de62ba4491ac8015f93
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2023-02-19 20:06:09 -0600

    deleted the brains of this page
    deleted the brains of this page. it was causing compilation issues, and it was not working anyway. Plus the page is not on the projects page yet.

--- src/components/GFreq/GFreq.js
+++ src/components/GFreq/GFreq.js
@@ -3,8 +3,8 @@ import Footer from '../Footer/Footer';
 import NavBar from './../NavBar/NavBar';
 import './GFreq.css';
 
-import getUserMedia from "get-user-media-promise";
-import MicrophoneStream from "microphone-stream";
+// import getUserMedia from "get-user-media-promise";
+// import MicrophoneStream from "microphone-stream";
 // import Pitchfinder from "pitchfinder";
 
 
@@ -15,84 +15,7 @@ function GFreq() {
     // const [audioNoteName, setAudioNoteName] = useState(0);
     // const [audioOctave, setAudioOctave] = useState(0);
     const [frequency, setFrequency] = useState(0);
-    const [note, setNote] = useState(0);
-
-
-
-
-    useEffect(() => {
-        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        const analyser = audioContext.createAnalyser();
-        const microphone = audioContext.createMediaStreamSource(navigator.mediaDevices.getUserMedia({ audio: true }));
-        const bufferSize = 2048;
-        const buffer = new Float32Array(bufferSize);
-    
-        analyser.fftSize = 2048;
-        analyser.smoothingTimeConstant = 0.8;
-        microphone.connect(analyser);
-    
-        const autoCorrelate = (buf, sampleRate) => {
-          const SIZE = buf.length;
-          const MAX_SAMPLES = Math.floor(SIZE / 2);
-          let bestOffset = -1, bestCorrelation = 0, lastCorrelation = 1, rms = 0, foundGoodCorrelation = false;
-          let correlations = new Array(MAX_SAMPLES);
-    
-          for (let i = 0; i < SIZE; i++) {
-            rms += buf[i] ** 2;
-          }
-          rms = Math.sqrt(rms / SIZE);
-          if (rms < 0.01) return -1;
-    
-          for (let offset = 0; offset < MAX_SAMPLES; offset++) {
-            let correlation = 0;
-            for (let i = 0; i < MAX_SAMPLES; i++) {
-              correlation += Math.abs(buf[i] - buf[i + offset]);
-            }
-            correlation = 1 - (correlation / MAX_SAMPLES);
-            correlations[offset] = correlation;
-            if ((correlation > 0.9) && (correlation > lastCorrelation)) {
-              foundGoodCorrelation = true;
-              if (correlation > bestCorrelation) {
-                bestCorrelation = correlation;
-                bestOffset = offset;
-              }
-            } else if (foundGoodCorrelation) {
-              const shift = (correlations[bestOffset + 1] - correlations[bestOffset - 1]) / correlations[bestOffset];
-              return sampleRate / (bestOffset + (8 * shift));
-            }
-            lastCorrelation = correlation;
-          }
-    
-          return (bestCorrelation > 0.01) ? sampleRate / bestOffset : -1;
-        };
-    
-        const getNoteFromFrequency = (frequency) => {
-          const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-          const A4 = 440;
-          const noteNumber = 12 * (Math.log(frequency / A4) / Math.log(2));
-          const roundedNote = Math.round(noteNumber);
-          const noteName = noteNames[roundedNote % 12];
-          const octave = Math.floor((roundedNote / 12) - 1);
-          return noteName + octave;
-        };
-    
-        const updatePitch = () => {
-          analyser.getFloatTimeDomainData(buffer);
-          const ac = autoCorrelate(buffer, audioContext.sampleRate);
-          if (ac !== -1) {
-            const frequency = audioContext.sampleRate / ac;
-            setFrequency(frequency);
-            setNote(getNoteFromFrequency(frequency));
-          }
-        };
-    
-        const updateInterval = setInterval(updatePitch, 100);
-    
-        return () => clearInterval(updateInterval);
-      }, []);
-    
-
-
+    // const [note, setNote] = useState(0);
 
 
 
@@ -116,7 +39,7 @@ function GFreq() {
                     {audioCents}
                     {audioNoteName}
                     {audioOctave} */}
-                    {note}
+                    {/* {note} */}
                     {frequency}
                 </div>
             </div>
