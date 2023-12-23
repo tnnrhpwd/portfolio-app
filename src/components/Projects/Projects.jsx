e4efcb67168bbfc140b11c80789e71bc3ada2269
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2023-12-23 14:51:37 -0500

    consolidated assets, improved code, and more

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,291 +1,96 @@
 import React from "react";
-// import pollsimg from "./polls.jpg";
-import wordleSimg from "./wordle-solver.PNG";
-import wordleimg from "./wordle.PNG";
-import annuitiesimg from "./Annuities.jpeg";
-// import draftingimg from "./drafting.jpg";
-import ethanolimg from "./ethanol.PNG";
-import gfreqimg from "./Gfreq.png";
-// import saloncwimg from "./saloncw.png";
-import planitimg from './planit.png';
-import sleepassistimg from './sleepassist.png';
-import passgenimg from './passgen.png';
-
 import Utility from "./Utility";
-
 import Footer from '../Footer/Footer';
-import BitcoinAPI from "../BitcoinAPI/BitcoinAPI.js";
 import NavBar from '../NavBar/NavBar';
 import "./Projects.css";
 
+const projectData = [
+  {
+    id: 1,
+    imgSrc: require("../../assets/wordle.png"),
+    alt: "the display for wordle",
+    url: "/wordle",
+    description: "Wordle game clone with custom word length. Wordle Solver included.",
+    utilities: ["code", "html", "react", "css", "netlify"],
+  },
+  {
+    id: 2,
+    imgSrc: require("../../assets/Gfreq.png"),
+    alt: "the display for gfreq",
+    url: "/gfreq",
+    description: "Analyze any audio from the microphone on your device.",
+    utilities: ["code", "html", "react", "css", "netlify"],
+  },
+  {
+    id: 4,
+    imgSrc: require("../../assets/passgen.png"),
+    alt: "the display for passgen",
+    url: "/passgen",
+    description: "Want a random password?",
+    utilities: ["code", "html", "react", "css", "netlify"],
+  },
+  {
+    id: 5,
+    imgSrc: require("../../assets/Annuities.jpeg"),
+    alt: "the display for my cad projects",
+    url: "/annuities",
+    description: "Calculate the effect of compound interest on your investment.",
+    utilities: ["code", "html", "react", "css", "netlify"],
+  },
+  {
+    id: 7,
+    imgSrc: require("../../assets/planit.png"),
+    alt: "the display for my planit project",
+    url: "https://mern-planit.netlify.app/",
+    description: "Do you have a plan you want to share with the world?",
+    utilities: ["code", "html", "react", "css", "mongo", "node", "netlify"],
+  },
+];
+
 function Projects() {
-
-  
-
-    return (<>
+  return (
+    <>
       <NavBar />
       <div className="projects-space">
-        <div className="projects-title">
-          Projects
-        </div>
-        
-        <div className="projects-description">
-          Click each image to visit the project. 
-        </div>
-        <div></div>
+        <div className="projects-title">Projects</div>
+        <div className="projects-description">Click each image to visit the project.</div>
         <div className="projects-holder">
-
-          {/* <div className="projects-div">
-            <div className="projects-p1">
-              <a href="/pollbox">
-                <img id="projects-p1-img" src={pollsimg} alt="the display for polling website"/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Create your own survey for other site viewers. No sign-in required!
-              <br/><br/>
-              <div className="utility-space">
-              <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/PollBox"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="firebase"/>
-                <Utility type="netlify"/>
+          {projectData.map((project) => (
+            <div className="projects-div" key={project.id}>
+              <div className={`projects-p`}>
+                <a href={project.url} rel="noopener noreferrer" target="_blank">
+                  <img id={`projects-p-img`} src={project.imgSrc} alt={project.alt} />
+                </a>
+              </div>
+              <div className="projects-div-text">
+                {project.description}
+                <br /><br />
+                <div className="utility-space">
+                  {project.utilities.map((utility) => (
+                    <Utility key={utility} type={utility} url={project.url} />
+                  ))}
+                </div>
               </div>
             </div>
-          </div> */}
-          
-          <div className="projects-div">
-            <div className="projects-p6">
-              <a href="/wordle">
-                <img id="projects-p6-img" src={wordleimg} alt="the display for wordle "/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Try to guess the answer! You can also choose the word length.
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/Wordle"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div>
-
-          <div className="projects-div">
-            <div className="projects-p6">
-              <a href="/gfreq">
-                <img id="projects-p6-img" src={gfreqimg} alt="the display for gfreq "/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Analyze any audio from the microphone on your device. 
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/Gfreq"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div>
-
-          <div className="projects-div">
-            <div className="projects-p10">
-              <a href="/sleepassist">
-                <img id="projects-p10-img" src={sleepassistimg} alt="the display for sleepassist "/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Unsure what time to go to sleep?
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/SleepAssist"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div>          
-          
-          <div className="projects-div">
-            <div className="projects-p11">
-              <a href="/passgen">
-                <img id="projects-p11-img" src={passgenimg} alt="the display for passgen "/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Want a random password?
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/PassGen"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div>
-
-          {/* <div className="projects-div">
-            <div className="projects-p8">
-              <a href="https://saloncw.netlify.app/"  rel="noopener noreferrer" target="_blank">
-                <img id="projects-p8-img" src={saloncwimg} alt="the display for saloncw "/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              This was designed for a local salon to provide information to potential customers.
-              <br/><br/>
-              <div className="utility-space">
-
-                <Utility type="code" url="https://github.com/tnnrhpwd/saloncw"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div> */}
-
-          <div className="projects-div">
-            <div className="projects-p5">
-              <a href="/annuities">
-                <img id="projects-p5-img" src={annuitiesimg} alt="the display for my cad projects"/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Calculate the effect of compound interest on your investment.
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/Annuities"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div>
-
-          <div className="projects-div">
-            <div className="projects-p2">
-              <a href="/wordlesolver">
-                <img id="projects-p2-img" src={wordleSimg} alt="the display for wordle solver website"/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Find the answer to any Wordle puzzle.
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/WordleSolver"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div>
-
-          <div className="projects-div">
-            <div className="projects-p9">
-              <a href="https://mern-planit.netlify.app/"  rel="noopener noreferrer" target="_blank">
-                <img id="projects-p9-img" src={planitimg} alt="the display for my planit project"/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Do you have a plan you want to share with the world?
-              <br/><br/>( Please wait for Render initial load )
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/mern-planit-app"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                {/* <Utility type="heroku"/> */}
-                <Utility type="mongo"/>
-                <Utility type="node"/>
-              </div>
-            </div>
-          </div>
-          
-          <div className="projects-div">
-            <div className="projects-p7">
-              <a href="/ethanol">
-                <img id="projects-p7-img" src={ethanolimg} alt="the display for ethanol calculator"/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              Calculates the standard drinks in any alcoholic volume.
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/Ethanol"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div>
-          {/* <div className="projects-div">
-            <div className="projects-p3">
-              <BitcoinAPI/>
-            </div>
-            <div className="projects-div-text">
-            <br/>
-              This displays the current bitcoin price using the CoinDesk API. Click the price to call an update.
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/BitcoinAPI"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div> */}
-
-          {/* <div className="projects-div">
-            <div className="projects-p4">
-              <a href="/drafting">
-                <img id="projects-p4-img" src={draftingimg} alt="the display for my cad projects"/>
-              </a>
-            </div>
-            <div className="projects-div-text">
-              This displays my previous drafting work in an image slideshow.
-              <br/><br/>
-              <div className="utility-space">
-                <Utility type="code" url="https://github.com/tnnrhpwd/portfolio-app/tree/master/src/components/Drafting"/>
-                <Utility type="html"/>
-                <Utility type="react"/>
-                <Utility type="css"/>
-                <Utility type="netlify"/>
-              </div>
-            </div>
-          </div> */}
+          ))}
         </div>
-
         <div></div>
         <div className="projects-resume">
           <a className="projects-resume-inside" href="https://docs.google.com/document/d/1l8yCRlom5hw-SwOfZtpria_AUuXwcXpC/edit?usp=sharing&ouid=106668374323360993837&rtpof=true&sd=true" rel="noopener noreferrer" target="_blank">
-                  <button id="projects-resume-button">STH RESUME</button>
+            <button id="projects-resume-button">STH RESUME</button>
           </a>
         </div>
         <div></div>
         <div className="projects-source">
           <a className="projects-source-inside" href="https://github.com/tnnrhpwd/portfolio-app" rel="noopener noreferrer" target="_blank">
-                  <button id="projects-source-button">This Website's Github Repo</button>
+            <button id="projects-source-button">This Website's Github Repo</button>
           </a>
         </div>
         <div></div>
-        
-
       </div>
-      <Footer/>
-    </>);
-  }
-  
-  export default Projects;
+      <Footer />
+    </>
+  );
+}
 
+export default Projects;
--- src/components/Projects/Utility.jsx
+++ src/components/Projects/Utility.jsx
@@ -1,133 +1,41 @@
 import ReactTooltip from "react-tooltip";
 import React from 'react';
-import codeimg from "./github.png";
-import htmlimg from "./html.png";
-import reactimg from "./react.png";
-import firebaseimg from "./firebase.png";
-import cssimg from "./css.png";
-import netlifyimg from "./netlify.png";
-import herokuimg from "./heroku.png";
-import mongoimg from "./mongo.png";
-import nodeimg from "./node.png";
 
-function Utility(props) {
-    var showTips;
+const logos = {
+  code: { img: require("../../assets/github.png"), tip: "Source Code" },
+  html: { img: require("../../assets/html.png"), tip: "HTML" },
+  react: { img: require("../../assets/react.png"), tip: "React JavaScript" },
+  css: { img: require("../../assets/css.png"), tip: "CSS" },
+  firebase: { img: require("../../assets/firebase.png"), tip: "Google Firebase" },
+  heroku: { img: require("../../assets/heroku.png"), tip: "Heroku" },
+  mongo: { img: require("../../assets/mongo.png"), tip: "MongoDB" },
+  node: { img: require("../../assets/node.png"), tip: "NodeJS" },
+  netlify: { img: require("../../assets/netlify.png"), tip: "Netlify" },
+};
 
-    switch(props.tips){
-        case true:
-            showTips = true;break;
-        case false:
-            showTips = false;break;
-        default:
-            showTips = true;break;
-    }
+function Utility({ type, tips = true, url }) {
+  const { img: LogoImg, tip } = logos[type] || {};
 
-    switch(props.type) {
-        case "code":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-code-tip">
-                    <a href={props.url}  rel="noopener noreferrer" target="_blank">
-                        <img className="projects-logos" id="projects-code" src={codeimg} alt="source code logo"/>
-                    </a>
-                </div>
-                {showTips && 
-                    <ReactTooltip id="projects-code-tip" place="bottom" effect="solid">
-                        <div className="projects-tip-words"> Source Code</div>
-                    </ReactTooltip>
-                }
-            </>);
-        case "html":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-html-tip">
-                    <img className="projects-logos" src={htmlimg} alt="html logo"/>
-                </div>
-                {showTips && 
-                    <ReactTooltip id="projects-html-tip" place="bottom" effect="solid">
-                        <div className="projects-tip-words"> HTML</div>
-                    </ReactTooltip>
-                }
-            </>);
-        case "react":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-react-tip">
-                    <img className="projects-logos" src={reactimg} alt="reactjs logo"/>
-                </div>
-                {showTips && 
-                    <ReactTooltip id="projects-react-tip" place="bottom" effect="solid">
-                        <div className="projects-tip-words"> React JavaScript</div>
-                    </ReactTooltip>
-                }
-            </>);
-        case "css":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-css-tip">
-                    <img className="projects-logos" src={cssimg} alt="css logo"/>
-                </div>
-                {showTips && 
-                    <ReactTooltip id="projects-css-tip" place="bottom" effect="solid">
-                    <div className="projects-tip-words"> CSS</div>
-                    </ReactTooltip>
-                }
-            </>);
-        case "firebase":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-firebase-tip">
-                    <img className="projects-logos" src={firebaseimg} alt="firebase logo"/>
-                </div>
-                {showTips &&
-                    <ReactTooltip id="projects-firebase-tip" place="bottom" effect="solid">
-                        <div className="projects-tip-words"> Google Firebase</div>
-                    </ReactTooltip>
-                }
-            </>);        
-        case "heroku":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-heroku-tip">
-                    <img className="projects-logos" src={herokuimg} alt="heroku logo"/>
-                </div>
-                {showTips &&
-                    <ReactTooltip id="projects-heroku-tip" place="bottom" effect="solid">
-                        <div className="projects-tip-words"> Heroku</div>
-                    </ReactTooltip>
-                }
-            </>);
-        case "mongo":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-mongo-tip">
-                    <img className="projects-logos" src={mongoimg} alt="mongo logo"/>
-                </div>
-                {showTips &&
-                    <ReactTooltip id="projects-mongo-tip" place="bottom" effect="solid">
-                        <div className="projects-tip-words"> MongoDB</div>
-                    </ReactTooltip>
-                }
-            </>);
-        case "node":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-node-tip">
-                    <img className="projects-logos" src={nodeimg} alt="node logo"/>
-                </div>
-                {showTips &&
-                    <ReactTooltip id="projects-node-tip" place="bottom" effect="solid">
-                        <div className="projects-tip-words"> NodeJS</div>
-                    </ReactTooltip>
-                }
-            </>);
-        case "netlify":
-            return (<>
-                <div className="projects-logos-div" data-tip="" data-for="projects-netlify-tip">
-                    <img className="projects-logos" src={netlifyimg} alt="netlify logo"/>
-                </div>
-                {showTips &&
-                    <ReactTooltip id="projects-netlify-tip" place="bottom" effect="solid">
-                        <div className="projects-tip-words"> Netlify</div>
-                    </ReactTooltip>
-                }
-            </>);
-        default:
-            return;
-    }
+  if (!LogoImg) return null;
 
+  return (
+    <>
+      <div className="projects-logos-div" data-tip="" data-for={`projects-${type}-tip`}>
+        {url ? (
+          <a href={url} rel="noopener noreferrer" target="_blank">
+            <img className="projects-logos" id={`projects-${type}`} src={LogoImg} alt={`${type} logo`} />
+          </a>
+        ) : (
+          <img className="projects-logos" src={LogoImg} alt={`${type} logo`} />
+        )}
+      </div>
+      {tips && (
+        <ReactTooltip id={`projects-${type}-tip`} place="bottom" effect="solid">
+          <div className="projects-tip-words">{tip}</div>
+        </ReactTooltip>
+      )}
+    </>
+  );
 }
 
-export default Utility
+export default Utility;
