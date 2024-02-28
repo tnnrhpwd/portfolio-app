1743735c89c90c557eb2775dda9ed1def739ca53
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2024-02-27 21:05:10 -0500

    Implemented trunkated goals

--- frontend/src/components/Simple/ActionResult/ActionResult.jsx
+++ frontend/src/components/Simple/ActionResult/ActionResult.jsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import './ActionResult.css';
 
-function ActionResult(props) {
-  return (<>
+function ActionResult(props) {// ActionResult component displays the result of an action. If the length of the action string exceeds 200 characters, it truncates the string and appends '...' to indicate truncation.
+  let displayText = props.selAction;
+  if (props.selAction.length > 200) {  // Check if the length of selAction exceeds 200 characters
+    displayText = props.selAction.slice(0, 200) + '...';    // If so, truncate the string and append '...' to indicate it's truncated
+  }
+  return (  // Render the component with the truncated or original text
     <div className='actionresult'>
-      {props.selAction}
+      {displayText}
     </div>
-  </>)
+  );
 }
 
-export default ActionResult
+export default ActionResult;
