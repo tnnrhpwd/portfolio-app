bbd34ceeef060dd7d6adf38f5d682e2384840e4a
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2024-07-14 20:44:36 -0400

    Implement graph only for power users and preent multiple intials renders in dev. mode. to do make the enitre app only render once on render

--- frontend/src/pages/Projects/Annuities/Annuities.jsx
+++ frontend/src/pages/Projects/Annuities/Annuities.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateData, getData, resetDataSlice, deleteData } from '../../../features/data/dataSlice.js';
 import { toast } from 'react-toastify';
@@ -16,179 +16,125 @@ function Annuities() {
 
     const dispatch = useDispatch();
     const [powerMode, setPowerMode] = useState(false);
+    const effectRan = useRef(false);
 
-    // Get the relevant data from the state
-    const { user, data, dataIsSuccess, dataIsLoading, dataIsError, dataMessage, operation } = useSelector(
-        (state) => state.data
-    );
+    const { user, data, dataIsSuccess } = useSelector((state) => state.data);
 
-    useEffect(() => {  // Additional useEffect for fetching data on component mount
-        async function getMyData() {
-          try {
-            if (!user || user === null) { //guard clause: no user
-                setPowerMode(false)
-                return;
-            }
-            if (!user._id || user._id === '64efe9e2c42368e193ee6977') { // guard clause: guest user
-                setPowerMode(false);
-                return;
-            }setPowerMode(true)
-            toast.success('Power user active.', {autoClose: toastDuration});
-            await dispatch(getData({ data: "Net:" }));
-          } catch (error) {
-            console.error(error);
-            toast.error(error, { autoClose: toastDuration });    
-          }
-        }
-        if (!dataIsSuccess){
-            getMyData();
-        }
+    useEffect(() => {
+        if (effectRan.current === false) {
+            // Your effect logic here
+            console.log("This will only run once");
+      
+            effectRan.current = true; 
+
+            const getMyData = async () => {
+                try {
+                    if (!user || !user._id || user._id === '64efe9e2c42368e193ee6977') {
+                        setPowerMode(false);
+                        return;
+                    }
+                    setPowerMode(true);
+                    toast.success('Power user active.', { autoClose: toastDuration });
+                    await dispatch(getData({ data: "Net:" }));
+                } catch (error) {
+                    toast.error(error, { autoClose: toastDuration });
+                }
+            };
     
-        // Reset the data slice when the component unmounts
+            if (!dataIsSuccess) {
+                getMyData();
+            }
+        }
+
+    
         return () => {
           dispatch(resetDataSlice());
         };
-    }, [dispatch]);
-
-    var crntAnswer=0;
+    }, [dispatch, toastDuration]);
 
     const [answer, setAnswer] = useState(0);
-    const [showNewAnnuity, setShowNewAnnuity] = useState(0);
+    const [showNewAnnuity, setShowNewAnnuity] = useState(true);
+    const [time, setTime] = useState('$Present');
+    const [annuityCall, setAnnuityCall] = useState([]);
 
-    const [time, setTime] = useState('$Present');       // holds the chosen return tense
-    const [annuityCall, setAnnuityCall] = useState([]); // holds the input variables from the input fields
+    useEffect(() => setAnswer(0), [time]);
 
-    // clear results on tense change
     useEffect(() => {
-        setAnswer(0);
-    }, [time])
+        const computeAnnuity = () => {
+            let crntAnswer = 0;
+            const [inputPresent, inputAnnual, inputFuture, inputGradient, inputInterest, inputPeriods] = annuityCall;
 
+            const calculations = {
+                "$Future": {
+                    "PTOF": () => inputPresent && (crntAnswer += inputPresent * Math.pow(1 + inputInterest, inputPeriods)),
+                    "ATOF": () => inputAnnual && (crntAnswer += inputAnnual * ((Math.pow(1 + inputInterest, inputPeriods) - 1) / inputInterest)),
+                    "GTOF": () => inputGradient && (crntAnswer += inputGradient * ((Math.pow(1 + inputInterest, inputPeriods) - 1) / (inputInterest ** 2) - inputPeriods / inputInterest))
+                },
+                "$Present": {
+                    "FTOP": () => inputFuture && (crntAnswer += inputFuture * Math.pow(1 / (1 + inputInterest), inputPeriods)),
+                    "ATOP": () => inputAnnual && (crntAnswer += inputAnnual * ((Math.pow(1 + inputInterest, inputPeriods) - 1) / (inputInterest * Math.pow(1 + inputInterest, inputPeriods)))),
+                    "GTOP": () => inputGradient && (crntAnswer += inputGradient * (1 / inputInterest) * ((Math.pow(1 + inputInterest, inputPeriods) - 1) / (inputInterest * Math.pow(1 + inputInterest, inputPeriods)) - inputPeriods / Math.pow(1 + inputInterest, inputPeriods)))
+                },
+                "$Periodic": {
+                    "FTOA": () => inputFuture && (crntAnswer += inputFuture * (inputInterest / (Math.pow(1 + inputInterest, inputPeriods) - 1))),
+                    "PTOA": () => inputPresent && (crntAnswer += inputPresent * inputInterest * Math.pow(1 + inputInterest, inputPeriods) / (Math.pow(1 + inputInterest, inputPeriods) - 1)),
+                    "GTOA": () => inputGradient && (crntAnswer += inputGradient * ((1 / inputInterest) - (inputPeriods / (Math.pow(1 + inputInterest, inputPeriods) - 1))))
+                }
+            };
 
-    // Parse input data + incorporate conversions into output variable(answer) -- do this every time a new annuity is submitted.
-    useEffect(() => {
-        // crntAnswer = answer;
-        let inputPresent=annuityCall[0];
-        let inputAnnual=annuityCall[1];
-        let inputFuture=annuityCall[2];
-        let inputGradient=annuityCall[3];
-        let inputInterest=annuityCall[4];
-        let inputPeriods=annuityCall[5];
+            Object.values(calculations[time] || {}).forEach(fn => fn());
 
-        //PTOF
-        if(inputPresent&&(time==="$Future")){
-            let ansr = (crntAnswer + (inputPresent * (Math.pow(1+inputInterest,inputPeriods))));
-            crntAnswer= ansr;
-        }
-        //ATOF
-        if(inputAnnual&&(time==="$Future")){
-            let ansr = (crntAnswer + (inputAnnual * (((Math.pow((1+inputInterest), inputPeriods))-1)/inputInterest)));
-            crntAnswer= ansr;
-        }
-        //GTOF
-        if(inputGradient&&(time==="$Future")){
-            let innL = (((Math.pow((1+inputInterest),inputPeriods)-1))/(inputInterest*inputInterest));
-            let innR = (inputPeriods/inputInterest);
-            let ansr = (crntAnswer + inputGradient*(innL-innR));
-            crntAnswer= ansr;
-        }
-        //FTOP
-        if(inputFuture&&(time==="$Present")){
-            let ansr = (crntAnswer + (inputFuture*(Math.pow(1/(1+inputInterest), inputPeriods))));
-            crntAnswer= ansr;
-        }
-        //ATOP
-        if(inputAnnual&&(time==="$Present")){
-            let ansr = (crntAnswer + (inputAnnual * (((Math.pow((1+inputInterest),inputPeriods))-1)/(inputInterest*(Math.pow((1+inputInterest),inputPeriods))))));
-            crntAnswer= ansr;
-        }
-        //GTOP
-        if(inputGradient&&(time==="$Present")){
-            let inL = ((Math.pow((1+inputInterest),inputPeriods)-1)/(inputInterest*(Math.pow((1+inputInterest),inputPeriods))));
-            let inR = inputPeriods/(Math.pow((1+inputInterest),inputPeriods));
-            let ansr = (crntAnswer + (inputGradient * (1/inputInterest)*(inL - inR)));
-            crntAnswer= ansr;
-        }
-        //FTOA
-        if(inputFuture&&(time==="$Periodic")){
-            let ansr = (crntAnswer + (inputFuture * ((inputInterest)/(Math.pow((1+inputInterest),inputPeriods)-1))));
-            crntAnswer= ansr;
-        }
-        //PTOA
-        if(inputPresent&&(time==="$Periodic")){
-            let upr = inputPresent*(inputInterest*(Math.pow((1+inputInterest),inputPeriods)));
-            let lwr = (Math.pow((1+inputInterest), inputPeriods)-1);
-            let ansr = (crntAnswer + (upr/lwr));
-            crntAnswer= ansr;
-        }
-        //GTOA
-        if(inputGradient&&(time==="$Periodic")){
-            let ansr = (crntAnswer + (inputGradient * ((1/inputInterest)-(inputPeriods/(Math.pow((1+inputInterest),inputPeriods)-1)))));
-            crntAnswer= ansr;
-        }
-        setAnswer(crntAnswer);
-    }, [annuityCall]);
+            setAnswer(crntAnswer);
+        };
 
-    const options = [
-        '$Present', '$Periodic', '$Future',
-    ];
+        computeAnnuity();
+    }, [annuityCall, time]);
+
+    const options = ['$Present', '$Periodic', '$Future'];
 
     const defaultOption = options[0];
 
-    return (
-    <>  
-        <Header/>
-            <div className='annuities'>
-            <div className='annuities-title'>
-                Annuities
-            </div>
-            <div className='annuities-subtitle'>
-                Financial Annuity Calculator
-            </div>
+    return (<>
+        <Header />
+        <div className='annuities'>
+            <div className='annuities-title'>Annuities</div>
+            <div className='annuities-subtitle'>Financial Annuity Calculator</div>
             <div className='annuities-inputs'>
-                <div className='annuities-find-text'>
-                    Find:
-                </div>
+                <div className='annuities-find-text'>Find:</div>
                 <div className='annuities-find-dropdown'>
-                    <Dropdown 
+                    <Dropdown
                         id="annuities-find-dropdown-id"
-                        options={options} 
-                        onChange={(e) => setTime(e.value)} 
-                        value={defaultOption} 
-                        placeholder="Select an option" 
+                        options={options}
+                        onChange={(e) => setTime(e.value)}
+                        value={time}
+                        placeholder="Select an option"
                     />
                 </div>
-                <button 
-                    onClick={()=>setShowNewAnnuity(showNewAnnuity+1)} 
-                    id='newAnnuity' 
-                    type='button'
-                    >
-                    Toggle New Visability
+                <button
+                    onClick={() => setShowNewAnnuity(!showNewAnnuity)}
+                    id='newAnnuity'
+                    type='button'>
+                    Toggle New Visibility
                 </button>
             </div>
 
             <div className='annuities-newannuity'>
-                {(showNewAnnuity%2===0) &&
-                    <NewAnnuity 
-                    tenseAnnuity={time} 
-                    onNewAnnuity={setAnnuityCall}  
-                    />
-                }
+                {showNewAnnuity && <NewAnnuity tenseAnnuity={time} onNewAnnuity={setAnnuityCall} />}
             </div>
 
-            {/* {!(answer===0)&&
+            {!(answer===0)&&(powerMode)&&
                 <div className='annuities-output'>
                     <span className='annuities-output-span'>
                         <GraphAnnuities chartValue={answer} chartData={annuityCall}   />
                     </span>
                 </div>
-            }   */}
+            }  
 
             <div className='annuities-resulttime'>
                 <div className='annuities-resulttime-text'>
                     The {time.substring(1)} Value would be ${answer.toFixed(2)}.
                 </div>
             </div>
-
 
             <div className='annuities-description'>
                 Example: If you have $500 and save $10 at 10% interest for 10 periods: Select $Future then enter 500 (present), 10 (periodic), 0.10 (interest), and 10 (periods).
