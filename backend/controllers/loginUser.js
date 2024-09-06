dfcb85c189070e9c6e585d277e76195a46a9cd6d
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2024-09-05 21:29:54 -0400

    Adding send email back to frontend

--- backend/controllers/loginUser.js
+++ backend/controllers/loginUser.js
@@ -2,33 +2,38 @@
 
 const asyncHandler = require('express-async-handler');
 const Data = require('../models/dataModel');
-const { generateToken } = require('./generateToken')
-const bcrypt = require('bcryptjs')  // used to hash passwords
+const { generateToken } = require('./generateToken');
+const bcrypt = require('bcryptjs');  // used to hash passwords
 
 // @desc    Authenticate a user
 // @route   POST /api/data/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
+    const { email, password } = req.body;
   
     // Check for user email
-    const user = await Data.findOne({ data: { $regex: `${email}` } })
-    if (user === null){
-      res.status(400)
-      throw new Error(email)  // showed in frontend 
+    const user = await Data.findOne({ data: { $regex: `${email}` } });
+    if (user === null) {
+      res.status(400);
+      throw new Error('Invalid email or password');  // Changed to a more generic error message
     }
+
+    // Extract password and nickname from the stored data
     const userPassword = user.data.substring(user.data.indexOf('|Password:') + 10);
-    const userNickname = user.data.substring(user.data.indexOf('Nickname:') + 9,user.data.indexOf('|Email:'))
-    if (user && (await bcrypt.compare(password, userPassword))) {  // if decrypted password equals user password input, send token back to user.
+    const userNickname = user.data.substring(user.data.indexOf('Nickname:') + 9, user.data.indexOf('|Email:'));
+    
+    // Check if the password matches
+    if (await bcrypt.compare(password, userPassword)) {
       res.json({
         _id: user.id,
-        nickname: userNickname,            // only need to send token back
+        email: email,  // Include email in the response
+        nickname: userNickname,
         token: generateToken(user._id),
-      })
+      });
     } else {
-      res.status(400)
-      throw new Error(userPassword)  // showed in frontend 
+      res.status(400);
+      throw new Error('Invalid email or password');  // Changed to a more generic error message
     }
-  })
+});
 
-  module.exports = { loginUser };
+module.exports = { loginUser };
